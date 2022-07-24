c3e46119ca2735733e29215dd924b4e60c567d46
91948 <91948@LAPTOP-0A5E04UV>
2022-07-24 14:39:37 -0400

    commit

--- src/test/java/StepDefinition/TestContext.java
+++ src/test/java/StepDefinition/TestContext.java
@@ -5,6 +5,17 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.xml.DOMConfigurator;
+//<<<<<<< HEAD
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import org.openqa.selenium.TakesScreenshot;
+
+import Utils.ExcelReader;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
@@ -14,8 +25,9 @@ import io.restassured.specification.RequestSpecification;
 
 public class TestContext {
 	
-	public  RequestSpecification requestSpec ;
-	public  Response response ;
+	public  RequestSpecification requestSpec = null;
+	public  Response response = null;
+//>>>>>>> branch 'master' of https://github.com/kumarisony18n/DieticianApiAutomation
 	public List<Map<String, String>> responseBody;
 	Scenario scn = null;
 	public HashMap<String, String> responsecoll;
